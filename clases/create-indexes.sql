73632fce76639fdf2ef8ac7cc1fabac16c261edf
adrianhmontero <hernandezmonteroadrian14@gmail.com>
2026-07-27 12:00:55 -0600

    Adding more indexes and creating a foreign key in city table

--- clases/create-indexes.sql
+++ clases/create-indexes.sql
@@ -3,3 +3,7 @@
 CREATE UNIQUE INDEX "unique_country_name" ON country (name);
 
 CREATE INDEX "country_continent" ON country (continent);
+
+CREATE UNIQUE INDEX"unique_name_countrycode_district" ON city(name,countrycode,district);
+
+CREATE INDEX "idx_district" ON city (district);
---
+++ clases/createForeignKey.sql
@@ -0,0 +1,9 @@
+ALTER TABLE city
+ADD CONSTRAINT fk_countrycode FOREIGN KEY (countrycode) REFERENCES country (code);
+
+SELECT * FROM country WHERE code ='NAM';
+SELECT * FROM city WHERE countrycode ='NAM';
+
+
+INSERT INTO country
+		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');

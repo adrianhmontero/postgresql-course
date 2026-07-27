f3053fa1a1528d7e4c9736c4892eb11342dc5a97
adrianhmontero <hernandezmonteroadrian14@gmail.com>
2026-07-27 12:51:51 -0600

    Deleting country AFG on cascade (cities and languages)

--- clases/createForeignKey.sql
+++ clases/createForeignKey.sql
@@ -7,3 +7,6 @@ SELECT * FROM city WHERE countrycode ='NAM';
 
 INSERT INTO country
 		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');
+
+        ALTER TABLE countrylanguage
+ADD CONSTRAINT fk_countrycode FOREIGN KEY (countrycode) REFERENCES country (code);

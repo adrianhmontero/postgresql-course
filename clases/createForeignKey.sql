634c64748aae30ebc2a16b3705651c22847c8cb7
adrianhmontero <hernandezmonteroadrian14@gmail.com>
2026-07-27 14:10:45 -0600

    Creating language table and foreign key on countrylanguage

--- clases/createForeignKey.sql
+++ clases/createForeignKey.sql
@@ -10,3 +10,50 @@ INSERT INTO country
 
         ALTER TABLE countrylanguage
 ADD CONSTRAINT fk_countrycode FOREIGN KEY (countrycode) REFERENCES country (code);
+
+INSERT INTO
+	country_bk
+SELECT
+	*
+FROM
+	country;
+
+ALTER TABLE country
+DROP CONSTRAINT country_continent_check;
+
+SELECT
+	a.name,
+	a.continent,
+	(
+		SELECT
+			"code"
+		FROM
+			continent b
+		WHERE
+			b.name = a.continent
+	)
+FROM
+	country a;
+
+UPDATE country a
+SET
+	continent = (
+		SELECT
+			"code"
+		FROM
+			continent b
+		WHERE
+			b.name = a.continent
+	);
+
+SELECT
+	*
+FROM
+	country;
+
+ALTER TABLE country
+ALTER COLUMN continent TYPE int4 USING continent::integer;
+
+
+ALTER TABLE country
+ADD CONSTRAINT fk_continent FOREIGN KEY (continent) REFERENCES continent (code);

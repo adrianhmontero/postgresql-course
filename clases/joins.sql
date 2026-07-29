02d211aeb259830558f6f9cca07f02e60616a909
adrianhmontero <hernandezmonteroadrian14@gmail.com>
2026-07-29 11:14:54 -0600

    Applying UNION between INNER JOIN and RIGHT OUTER JOIN queries

--- clases/joins.sql
+++ clases/joins.sql
@@ -26,3 +26,43 @@ FROM
 	RIGHT JOIN continent b ON a.continent = b.code
 WHERE
 	a.continent IS NULL;
+
+SELECT
+	count(*),
+	b.name
+FROM
+	country a
+	INNER JOIN continent b ON a.continent = b.code
+GROUP BY
+	b.name
+ORDER BY
+	count(*) ASC;
+
+(
+	SELECT
+		count(*) AS count,
+		b.name
+	FROM
+		country a
+		JOIN continent b ON a.continent = b.code
+	GROUP BY
+		b.name
+)
+UNION
+(
+	SELECT
+		-- Le ponemos cero porque, al hacer un right outer join sabemos que no
+		-- existe ningún registro en la tabla 'a'. Si no forzamos el cero, mostrará
+		-- el valor 1 porque encuentra un registro en la tabla 'b'.
+		0 AS count,
+		b.name
+	FROM
+		country a
+		RIGHT OUTER JOIN continent b ON a.continent = b.code
+	WHERE
+		a.continent IS NULL
+	GROUP BY
+		b.name
+)
+ORDER BY
+	count;

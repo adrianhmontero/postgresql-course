9649d074114bef31a84e5a116c672cacc101f0c8
adrianhmontero <hernandezmonteroadrian14@gmail.com>
2026-07-28 19:31:46 -0600

    Using INNER JOIN to get countries by continent name

--- clases/joins.sql
+++ clases/joins.sql
@@ -1,3 +1,10 @@
 select a.name as country, b."name" as continent from country a, continent b
 WHERE
 a.continent = b.code;
+
+SELECT
+	a.name AS country,
+	b.name AS continent
+FROM
+	country a
+	INNER JOIN continent b ON a.continent = b.code;
